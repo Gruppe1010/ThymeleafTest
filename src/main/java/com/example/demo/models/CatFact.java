20350ec697a6f2d70f38d4891424460d27214a86
Vibs <vibej@hotmail.com>
2020-09-24 14:31:25 +0200

    unødvendig metode slettet

--- src/main/java/com/example/demo/models/CatFact.java
+++ src/main/java/com/example/demo/models/CatFact.java
@@ -3,7 +3,7 @@ package com.example.demo.models;
 
 import java.util.List;
 
-// dette er en model - den modelererer et CatFact
+// dette er en model - den modellererer et CatFact
 // factText vil vi gerne have ind fra brugeren
 public class CatFact
 {
@@ -37,17 +37,4 @@ public class CatFact
     {
         this.factText = factText;
     }
-    
-    public String printList(List<CatFact> catFactList)
-    {
-        String catFactListString = "";
-        for(int i = 0; i < catFactList.size(); i++)
-        {
-            // int j = 1 + i;
-            catFactListString =
-                    catFactListString + "CatFact number: " + (i + 1) + " " + catFactList.get(i).getFactText();
-        }
-        return catFactListString;
-    }
-    
 }
